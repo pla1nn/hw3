24166144ea66d3b0e4d0dc933af989c9410f06d0
pla1n <danilkuisis@gmail.com>
2024-08-13 19:43:47 +0300

    asd

--- src/js/task1.js
+++ src/js/task1.js
@@ -1,2 +1,28 @@
-const img = document.querySelectorAll('.img');
+const imgs = document.querySelectorAll('img[data-src]');
 
+const observer = new IntersectionObserver(onEntry)
+
+function onEntry(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            observer.unobserve(entry.target);
+        }
+    });
+}
+
+function preloadImage(img) {
+    const src = img.getAttribute('data-src');
+    if (!src) {
+        return
+    } else {
+        img.src = src;
+        img.classList.add('show')
+    }
+}
+
+imgs.forEach(img => {
+    observer.observe(img);
+})
